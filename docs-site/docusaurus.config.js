1dc36a1758a89cc70f69dbf533721ab0db350c84
Kannan Kalidasan <kannanpoem1984@gmail.com>
2026-05-18 01:59:59 +0100

    Use explicit docs color modes

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: true,
+        respectPrefersColorScheme: false,
       },
       navbar: {
         title: 'dbt-charts',
