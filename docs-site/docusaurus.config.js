f8404b2587cf21a8b298cf283bc5baad4ff10aa8
Kannan Kalidasan <kannanpoem1984@gmail.com>
2026-05-31 16:49:55 +0100

    glyf studio section added

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
             type: 'html',
             position: 'right',
             value:
-              '<a class="navbarGithubLink" href="https://github.com/kannandreams/glyf" aria-label="Open Glyf on GitHub"><span>GitHub</span><svg class="navbarExternalIcon" viewBox="0 0 24 24" aria-hidden="true"><path d="M7 7H17V17" fill="none" stroke="currentColor" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round"/><path d="M17 7L6 18" fill="none" stroke="currentColor" stroke-width="2.4" stroke-linecap="round"/></svg></a>',
+              '<a class="navbarGithubLink" href="https://github.com/kannandreams/glyf" aria-label="View Glyf on GitHub"><svg class="navbarGithubMark" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.9.58.11.79-.25.79-.56 0-.28-.01-1.2-.02-2.18-3.2.7-3.88-1.36-3.88-1.36-.52-1.33-1.28-1.68-1.28-1.68-1.05-.72.08-.71.08-.71 1.15.08 1.76 1.18 1.76 1.18 1.03 1.75 2.69 1.25 3.34.96.1-.74.4-1.25.73-1.54-2.55-.29-5.23-1.28-5.23-5.68 0-1.25.45-2.27 1.17-3.07-.12-.29-.51-1.46.11-3.04 0 0 .96-.31 3.14 1.17a10.9 10.9 0 0 1 5.72 0c2.18-1.48 3.14-1.17 3.14-1.17.62 1.58.23 2.75.11 3.04.73.8 1.17 1.82 1.17 3.07 0 4.41-2.69 5.39-5.26 5.67.41.35.78 1.05.78 2.12 0 1.53-.01 2.76-.01 3.13 0 .31.21.68.8.56A11.5 11.5 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z" fill="currentColor"/></svg><span>View on GitHub</span></a>',
           },
         ],
       },
